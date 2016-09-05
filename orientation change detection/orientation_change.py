79e1a577550cb274f6f20fff0d86c1d015df131c
alexfromapex@gmail.com <alexfromapex@gmail.com>
2016-09-05 19:19:43 -0400

    Added blob, circle, moving object, orientation detection

--- orientation change detection/orientation_change.py
+++ orientation change detection/orientation_change.py
@@ -6,46 +6,10 @@ cam = cv2.VideoCapture(0)
 while True:
     ret,frame = cam.read()
     ret2,frame2 = cam.read()
+    frame3 = cv2.subtract(frame,frame2)
+    cv2.imshow('Orienation Change Detection',frame3)
 
-    # Convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
-    # Remove noise
-    frame = cv2.medianBlur(frame,11)
-    frame2 = cv2.medianBlur(frame2,11)
-
-    # Movement detection via visbile light change
-    diff_frame = cv2.subtract(gray,gray2)
-
-    # Setup SimpleBlobDetector parameters
-    params = cv2.SimpleBlobDetector_Params()
-     
-    # Thresholds
-    params.minThreshold = 1
-    params.maxThreshold = 255
-     
-    # Filter by Area
-    params.filterByArea = True
-    params.minArea = 50
-
-    # Distance between blobs
-    params.minDistBetweenBlobs = 130
-
-    # Filter by Convexity
-    params.filterByConvexity = True
-    params.minConvexity = 0.5
-
-    # Set up the detector with parameters
-    detector = cv2.SimpleBlobDetector(params)
-     
-    # Detect blobs
-    keypoints = detector.detect(diff_frame)
-
-    # Draw detected blobs
-    im_blobs = cv2.drawKeypoints(diff_frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    cv2.imshow('Detection',im_blobs)
+    print(frame3[0,0])
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
